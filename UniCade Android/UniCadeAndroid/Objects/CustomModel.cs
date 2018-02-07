435b983d17e8c2480a54e82eeb87e2c6acc14f1f
Ben Lenington <ben2lening@gmail.com>
2018-02-07 10:42:41 -0600

    Create CustomModel class for the new ListView

---
+++ UniCade Android/UniCadeAndroid/Objects/CustomModel.cs
@@ -0,0 +1,14 @@
+﻿namespace UniCadeAndroid.Objects
+{
+    public class CustomModel
+
+    {
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
