a487668dc9e227ab9f36da8875132dd64421e7f6
Ben Lenington <ben2lening@gmail.com>
2019-08-13 13:28:20 -0700

    Support deleting individual game images

--- UniCadeAndroid/Activities/FullscreenImageActivity.cs
+++ UniCadeAndroid/Activities/FullscreenImageActivity.cs
@@ -1,15 +1,11 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿using System.IO;
 using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using UniCadeAndroid.Constants;
 
 namespace UniCadeAndroid.Activities
 {
@@ -58,8 +54,20 @@ namespace UniCadeAndroid.Activities
 
             _deleteImageButton.Click += (sender, e) =>
             {
-                Toast.MakeText(ApplicationContext, "Delete Image", ToastLength.Long).Show();
-                //TODO: Delete image
+                
+                var sdCardPath = Environment.ExternalStorageDirectory.Path;
+                string consoleName = MainActivity.CurrentGame.ConsoleName;
+                string gameName = MainActivity.CurrentGame.ConsoleName;
+				string filePath = sdCardPath + ConstPaths.GameImagesPath + consoleName + "/" + gameName + "_BoxFront.jpg";
+				if (File.Exists(filePath))
+				{
+                    File.Delete(filePath);
+                    Toast.MakeText(ApplicationContext, "Image deleted", ToastLength.Long).Show();
+				}
+                else{
+                    Toast.MakeText(ApplicationContext, "Could not delete imgae", ToastLength.Long).Show();
+                }
+                Finish();
             };
 
             _closeButton.Click += (sender, e) =>
